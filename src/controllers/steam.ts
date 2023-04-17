460e65a927442f8a3ab33930c9a6ab57e85c97e6
MoizYousuf <moizyousuf24@gmail.com>
2023-04-17 10:46:11 +0500

    set api

--- src/controllers/steam.ts
+++ src/controllers/steam.ts
@@ -1,8 +1,9 @@
 import { PopulateHint } from "@mikro-orm/core";
 import express, { Handler } from "express";
-import { Profile, SteamAchievements, SteamFriend, SteamOwnedGames, Wallets } from "../models";
+import { Profile, SteamAchievements, SteamFriend, SteamGame, SteamOwnedGames, Wallets } from "../models";
 import { Request } from "../types";
 import { ResponseHelper } from "../utils";
+import { authenticate } from "../middlewares";
 
 const getFriends: Handler = (req: Request, res) => {
   const response = new ResponseHelper(res);
@@ -12,11 +13,11 @@ const getFriends: Handler = (req: Request, res) => {
       profile: {
         $or: [
           {
-            address: req.params.address,
+            useraddress: req.user.address,
           },
           {
             wallets: {
-              $like: `%${req.params.address}%`
+              $like: `%${req.user.address}%`
             }
           }
         ]
@@ -31,31 +32,55 @@ const getFriends: Handler = (req: Request, res) => {
 };
 const getGames: Handler = (req: Request, res) => {
   const response = new ResponseHelper(res);
-
+  console.log("req.user", req.user)
   return req.orm?.em
     .find(SteamOwnedGames, {
       profile: {
         $or: [
           {
-            address: req.params.address,
+            useraddress: req.user.address,
           },
           {
             wallets: {
-              $like: `%${req.params.address}%`
+              $like: `%${req.user.address}%`
             }
           }
         ]
       }
     }, {
-      populate: ["steamGame"],
+      populate: ["steamGame", "app_id"],
     })
     .then((games) => {
       if (!games) return response.notFound();
+      req.orm?.em.find(SteamGame, { app_id: games.map((item) => item.app_id) }).then((data) => {
+        req.orm?.em.find(SteamAchievements, {
+          profile: {
+            $or: [
+              {
+                useraddress: req.user.address,
+              },
+              {
+                wallets: {
+                  $like: `%${req.user.address}%`
+                }
+              }
+            ]
+          }, app_id: games.map((item) => item.app_id)
+        }).then((achievements) => {
+          // merge the data
+          let mergedData = games.map((item) => {
+            const game = data.find((game) => game.app_id === item.app_id);
+            const achievement = achievements.filter((achievement) => achievement.app_id === item.app_id) || {};
+            if (game) return { ...item, ...game, achievements: achievement }
+            else return item;
+          });
 
-      const total = games.reduce((acc, obj) => acc + obj.playtimeForever, 0);
-      const average = total / games.length;
-      // const friendsNew = games.toJSON();
-      return response.ok(undefined, { games, averagePlayTimePerGame: `${(average / 60).toFixed(0)} H`, numberOfGamesPlayed: games.filter((item) => item.playtimeForever).length, totalPlayTime: `${(total / 60).toFixed(0)} H` });
+          const total = games.reduce((acc, obj) => acc + obj.playtimeForever, 0);
+          const average = total / games.length;
+          // const friendsNew = games.toJSON();
+          return response.ok(undefined, { games: mergedData, averagePlayTimePerGame: `${(average / 60).toFixed(0)} H`, numberOfGamesPlayed: games.filter((item) => item.playtimeForever).length, totalPlayTime: `${(total / 60).toFixed(0)} H` });
+        });
+      })
     })
     .catch((e) => response.error(e.message));
 };
@@ -66,11 +91,11 @@ const getGamesAchievements: Handler = (req: Request, res) => {
       profile: {
         $or: [
           {
-            address: req.params.address,
+            useraddress: req.user.address,
           },
           {
             wallets: {
-              $like: `%${req.params.address}%`
+              $like: `%${req.user.address}%`
             }
           }
         ]
@@ -91,8 +116,8 @@ const getGamesAchievements: Handler = (req: Request, res) => {
 
 const router = express.Router();
 
-router.get("/friends/:address", getFriends);
-router.get("/games/:address", getGames);
-router.get("/games/achievements/:address/:gameId", getGamesAchievements);
+router.get("/friends", authenticate, getFriends);
+router.get("/games", authenticate, getGames);
+router.get("/games/achievements/:gameId", authenticate, getGamesAchievements);
 
 export default router;
