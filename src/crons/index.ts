15756467d677a2477a8382cdd99a2f122732d65e
moizyousuf24@gmail.com <moizyousuf24@gmail.com>
2023-05-18 12:46:07 +0500

    removed crons error

--- src/crons/index.ts
+++ src/crons/index.ts
@@ -1,11 +1,11 @@
-import cron from "node-cron";
-import collectibles from "./collectibles";
+// import cron from "node-cron";
+// import collectibles from "./collectibles";
 
 export default function () {
 
-    collectibles().catch((e: any) => console.error("[CRON] collectibles", e));
-    // Run Every Day at 12:00 AM
-    cron.schedule("0 0 * * *", () => {
-        collectibles().catch((e) => console.error("[CRON] collectibles", e));
-    });
+    // collectibles().catch((e: any) => console.error("[CRON] collectibles", e));
+    // // Run Every Day at 12:00 AM
+    // cron.schedule("0 0 * * *", () => {
+    //     collectibles().catch((e) => console.error("[CRON] collectibles", e));
+    // });
 }
